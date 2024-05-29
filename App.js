d6ebbd91c0acd6897c0782f7741856d7b96c20f5
Venkata Dathatreya K <dathatreyakv@gmail.com>
2024-05-30 01:56:56 +0530

    removing inline react script urls and added react through packages

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 // App.js
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const headerTag = document.createElement('h1');
 // headerTag.innerHTML = 'Hello world from Javascript!';
 // const rootTag = document.getElementById('root');
@@ -14,8 +15,8 @@ root.render(heading);
 
 const tag1 = React.createElement('div', {id: 'parent'},
   React.createElement('div', {id: 'child'},
-    [ React.createElement('h1', {}, "This is H1 tag"),
-      React.createElement ('h2', {}, "This is H2 tag")
+    [ React.createElement('h2', {key: 'header2'}, "This is H1 tag"),
+      React.createElement ('h3', {key: 'header3'}, "This is H2 tag")
     ]
   )
 )
