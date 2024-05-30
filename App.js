8c1957a7fe814db5c9a68f6cb2f07953e6061a69
Venkata Dathatreya K <dathatreyakv@gmail.com>
2024-05-30 21:37:29 +0530

    Component Composition

--- App.js
+++ App.js
@@ -1,34 +1,18 @@
 // App.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const headerTag = document.createElement('h1');
-// headerTag.innerHTML = 'Hello world from Javascript!';
-// const rootTag = document.getElementById('root');
-// rootTag.appendChild(headerTag);
 
-
-// const i1 = React.createElement('i', {}, "Hello world from React JS!")
-// const heading = React.createElement('h1', {id: 'header'}, i1);
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(heading);
-
-// const tag1 = React.createElement('div', {id: 'parent'},
-//   React.createElement('div', {id: 'child'},
-//     [ React.createElement('h2', {key: 'header2'}, "This is H1 tag"),
-//       React.createElement ('h3', {key: 'header3'}, "This is H2 tag")
-//     ]
-//   )
-// )
-// const root2 = ReactDOM.createRoot(document.getElementById('root2'))
-// setTimeout(() => root2.render(tag1), 2000)
-
+// component composition: is nothing by Adding/Using one component inside another component
 
 /** Babel transpiles this JSX into=> React.createElement =>
 * React.createElement => creates ReactElement JS object
 * JS object => ReactDOM renders this objects as HTML DOM elements
 */
-const h1Tag = <h1 id="header" className="sample">
-  This looks like HTML, but not. This is JS-XML which converts to HTML DOM element by multiple stages!🚀
-  </h1>
+const Title = () => <h1>Sample Title</h1>;
+const HeaderComponent = () => <>
+  <Title />
+  <h2>Subtile from Header HeaderComponent</h2>
+</>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(h1Tag);
+root.render(<HeaderComponent />);
