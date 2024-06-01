eca717db6c664096cc5fd2e6cf14b4629ab041e0
Venkata Dathatreya K <dathatreyakv@gmail.com>
2024-06-01 09:04:49 +0530

    typo

--- App.js
+++ App.js
@@ -33,14 +33,21 @@ const Header = () => (
   </div>
 )
 
-const restaurants = [1,2,3,4,5].map((i) => ({
+const restaurants = [{
   name: 'shah ghouse Foods',
   specials: 'Biryani, North Indian, Asian',
   rating: '4.3 Star',
   delivery_time: '38 minutes',
   logo: 'https://b.zmtcdn.com/data/pictures/3/93043/b4f8daac26d882ac8c0961932ba0b4a4_featured_v2.jpg',
   description: 'Restaurant Description'
-}))
+}, {
+  name: 'KFC',
+  specials: 'Smoke grill Chicken, Asian',
+  rating: '4 Star',
+  delivery_time: '25 minutes',
+  logo: 'https://b.zmtcdn.com/data/pictures/3/93043/b4f8daac26d882ac8c0961932ba0b4a4_featured_v2.jpg',
+  description: 'Restaurant Description'
+}]
 
 const RestaurantCard = ({logo, name, specials, rating, delivery_time}) => (
   <div className="res-card" style={{backgroundColor: '#F0F0F0'}}>
@@ -62,7 +69,7 @@ const Body = () => (
     </div>
     <div className="restaurant-container">
       {restaurants.map((restaurant) => (
-        <RestaurantCard {...restaurants[0]}/>
+        <RestaurantCard {...restaurant}/>
       ))}
     </div>
   </div>
