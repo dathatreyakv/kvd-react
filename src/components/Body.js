b42fd2c1e605d09fe988b5e48bbc1678c2013814
Venkata Dathatreya K <dathatreyakv@gmail.com>
2024-06-02 17:29:52 +0530

    using useState react hook- to filter the restaurants based on rating

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,28 @@
 import restaurantsList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import {useState} from "react";
 
-const Body = () => (
-  <div className="body">
-    <div className="search">
-      <input type="text" placeholder="Search Restaurants"/>
-      Search
+const Body = () => {
+  const [restaurants , setRestaurants] = useState(restaurantsList);
+
+  function filterTopRatedRestaurants() {
+    setRestaurants(restaurantsList.filter((restaurant) => restaurant?.info?.avgRating >= 4.5) )
+  }
+
+  return <div className="body">
+    <div className="data-filters">
+      <div className="search">
+        <input type="text" placeholder="Search Restaurants"/>
+        Search
+      </div>
+      <button type="button" className="btn btn-rating-filter" onClick={filterTopRatedRestaurants}>Top rated Restaurants</button>
     </div>
+    
+
     <div className="restaurant-container">
-      {  restaurantsList.map((restaurant) => <RestaurantCard key={restaurant?.info?.id} data={restaurant}/>) }
+      {  restaurants.map((restaurant) => <RestaurantCard key={restaurant?.info?.id} data={restaurant}/>) }
     </div>
   </div>
-)
+}
 
 export default Body;
