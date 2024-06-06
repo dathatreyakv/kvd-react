9c5a1f842ae22dd8532ac8522daa3ff8f6253e3c
Venkata Dathatreya K <dathatreyakv@gmail.com>
2024-06-06 23:05:40 +0530

    adding Lazy loadng to components import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // App.js
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import About from "./components/About";
 import ContactUs from "./components/ContactUs";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu.js";
+
+const About = lazy(()=> import("./components/About"));
 
 const AppLayout = () => (
   <div className="app">
@@ -28,7 +29,9 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About/>
+        element:  <Suspense fallback={<h1>Loading Loading...</h1>}>
+            <About/>
+          </Suspense>
       },
       {
         path: "/contact",
